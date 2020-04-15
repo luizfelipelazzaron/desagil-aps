d422c760f1dda73c3c91c3da8e4e38d63937eddb
Thalia Loiola <thalials@al.insper.edu.br>
2020-04-15 16:38:10 -0300

    ajeitando a estrutura

--- src/test/java/br/pro/hashi/ensino/desagil/aps/model/EmitterFalse.java
+++
@@ -1,8 +0,0 @@
-package br.pro.hashi.ensino.desagil.aps.model;
-
-public class EmitterFalse implements Emitter {
-    @Override
-    public boolean read() {
-        return false;
-    }
-}
--- src/test/java/br/pro/hashi/ensino/desagil/aps/model/EmitterTrue.java
+++
@@ -1,8 +0,0 @@
-package br.pro.hashi.ensino.desagil.aps.model;
-
-public class EmitterTrue implements Emitter {
-    @Override
-    public boolean read() {
-        return true;
-    }
-}
